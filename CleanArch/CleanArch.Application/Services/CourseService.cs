83c879d8ed9a42a0343d19d4091ea705be8109c2
vedpbehera <vxbehera@sentara.com>
2020-04-24 21:49:53 +0530

    Updated course view model

--- CleanArch/CleanArch.Application/Services/CourseService.cs
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using System.Text;
 using CleanArch.Application.Interfaces;
 using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.Core.Bus;
 using CleanArch.Domain.Interfaces;
 
 namespace CleanArch.Application.Services
 {
     public class CourseService : ICourseService
     {
-        private ICourseRepository _courseRepository;
-        public CourseService(ICourseRepository courseRepository)
+        private readonly ICourseRepository _courseRepository;
+        private readonly IMediatorHandler _bus;
+        public CourseService(ICourseRepository courseRepository, IMediatorHandler bus)
         {
             _courseRepository = courseRepository;
+            _bus = bus;
         }
         public CourseViewModel GetCourses()
         {
@@ -20,6 +24,14 @@ namespace CleanArch.Application.Services
             {
                 Courses = _courseRepository.GetCourses()
             };
+        }
+
+        public void CreateCourse(CourseViewModel courseViewModel)
+        {
+            var createCourseCommand = new CreateCourseCommand(
+                courseViewModel.Name, courseViewModel.Description, courseViewModel.ImageUrl
+                );
+            _bus.SendCommand(createCourseCommand);
         }
     }
 }
--- CleanArch/CleanArch.Application/ViewModels/CourseViewModel.cs
+++ CleanArch/CleanArch.Application/ViewModels/CourseViewModel.cs
@@ -7,6 +7,11 @@ namespace CleanArch.Application.ViewModels
 {
     public class CourseViewModel
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+
         public IEnumerable<Course> Courses { get; set; }
     }
 }
